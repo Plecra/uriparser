03343b37502fcbf5a4463d9726d66d462043b88b
Marli Frost <marli@frost.red>
2021-04-01 22:40:01 +0100

    feat: add uri inspection APIs

--- examples/parse.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let uri = uriparser::Uri::parse(b"https://www.youtube.com/watch?v=HOJ1NVtlnyQ")
-        .unwrap()
-        .into_owned();
-    println!("{}", uri.query());
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,72 @@
-use uriparser_sys;
-use core::ptr;
-unsafe fn text_range_as_bytes<'a>(range: &uriparser_sys::UriTextRangeA) -> Option<&'a [u8]> {
+//! These APIs are all pretty rough for now - only use them after verifying their safety
+
+use uriparser_sys::{self, UriPathSegmentA, UriTextRangeA, UriUriA, uriToStringA, uriToStringCharsRequiredA};
+use core::{fmt, ptr, cmp};
+fn bool_to_uri(b: bool) -> uriparser_sys::UriBool {
+    if b {
+        uriparser_sys::URI_TRUE as i32
+    } else {
+        uriparser_sys::URI_FALSE as i32
+    }
+}
+pub fn escape<'o>(text: &[u8], out: &'o mut [u8], replace_space: bool, normalize_line_breaks: bool) -> &'o mut [u8] {
+    assert!(out.len() >= text.len() * if normalize_line_breaks {
+        6
+    } else {
+        3
+    } + 1);
+    let end = unsafe {
+        uriparser_sys::uriEscapeExA(
+            text.as_ptr() as *const _, 
+            text.as_ptr().add(text.len()) as *const _,
+            out.as_mut_ptr() as *mut _, 
+            bool_to_uri(replace_space), 
+            bool_to_uri(normalize_line_breaks)
+        )
+    };
+    let new_len = end as usize - out.as_ptr() as usize;
+    &mut out[..new_len]
+}
+pub enum LineBreak {
+    Cr,
+    Lf,
+    Crlf,
+}
+fn hex(v: u8) -> Option<u8> {
+    match v {
+        b'a'..=b'f' => Some(v - b'a' + 10),
+        b'A'..=b'F' => Some(v - b'A' + 10),
+        b'0'..=b'9' => Some(v - b'0'),
+        _ => None
+    }
+}
+pub fn unescape(text: &mut [u8], replace_plus: bool, line_breaks: LineBreak) -> &mut [u8] {
+    let last_byte = match text {
+        [.., p, a, b] if *p == b'%' => {
+            if let Some(n) = hex(*a).and_then(|a| hex(*b).map(|b| a * 16 + b)) {
+                *p = 0;
+                n
+            } else {
+                core::mem::replace(b, 0)
+            }
+        }
+        [.., l] => core::mem::replace(l, 0),
+        [] => return text,
+    };
+    let last = unsafe {
+        uriparser_sys::uriUnescapeInPlaceExA(text.as_mut_ptr() as *mut _, bool_to_uri(replace_plus), match line_breaks {
+            LineBreak::Cr => uriparser_sys::UriBreakConversionEnum_URI_BR_TO_CR,
+            LineBreak::Lf => uriparser_sys::UriBreakConversionEnum_URI_BR_TO_LF,
+            LineBreak::Crlf => uriparser_sys::UriBreakConversionEnum_URI_BR_TO_CRLF,
+        })
+    };
+    unsafe {
+        *(last as *const u8 as *mut u8) = last_byte;
+    }
+    let new_len = last as usize - text.as_ptr() as usize;
+    &mut text[..=new_len]
+}
+unsafe fn text_range_as_bytes<'a>(range: &UriTextRangeA) -> Option<&'a [u8]> {
     if range.first.is_null() {
         None
     } else {
@@ -10,9 +76,9 @@ unsafe fn text_range_as_bytes<'a>(range: &uriparser_sys::UriTextRangeA) -> Optio
         ))
     }    
 }
-
+#[derive(Debug)]
 pub struct Uri<'a> {
-    raw: uriparser_sys::UriUriA,
+    raw: UriUriA,
     marker: core::marker::PhantomData<&'a [u8]>,
 }
 
@@ -57,6 +123,34 @@ impl<'a> Uri<'a> {
             err => Err(ParseError::from_code(err, errpos as usize - uri.as_ptr() as usize)),
         }
     }
+    pub fn resolve(&self, reference: &Uri<'_>, strict: bool) -> Result<Uri<'static>, ()> {
+        let mut out = Default::default();
+        match unsafe {
+            uriparser_sys::uriAddBaseUriExA(&mut out, &reference.raw, &self.raw, if strict {
+                uriparser_sys::UriResolutionOptionsEnum_URI_RESOLVE_STRICTLY
+            } else {
+                uriparser_sys::UriResolutionOptionsEnum_URI_RESOLVE_IDENTICAL_SCHEME_COMPAT
+            })
+        } as u32 {
+            uriparser_sys::URI_SUCCESS => Ok(Uri {
+                raw: out,
+                marker: core::marker::PhantomData,
+            }),
+            err => Err(())
+        }
+    }
+    pub fn as_relative(&self, base: &Uri<'_>, from_domain_root: bool) -> Result<Uri<'static>, ()> {
+        let mut out = Default::default();
+        match unsafe {
+            uriparser_sys::uriRemoveBaseUriA(&mut out, &self.raw, &base.raw, bool_to_uri(from_domain_root))
+        } as u32 {
+            uriparser_sys::URI_SUCCESS => Ok(Uri {
+                raw: out,
+                marker: core::marker::PhantomData,
+            }),
+            err => Err(())
+        }
+    }
     pub fn into_owned(mut self) -> Uri<'static> {
         unsafe {
             uriparser_sys::uriMakeOwnerA(&mut self.raw);
@@ -66,20 +160,139 @@ impl<'a> Uri<'a> {
             marker: core::marker::PhantomData,
         }
     }
-    pub fn scheme(&self) -> &str {
-        core::str::from_utf8(unsafe { text_range_as_bytes(&self.raw.scheme) }.unwrap()).unwrap()
+    pub fn scheme(&self) -> Option<&str> {
+        unsafe { text_range_as_bytes(&self.raw.scheme) }.map(|s| core::str::from_utf8(s).unwrap())
     }
-    pub fn userinfo(&self) -> &str {
-        core::str::from_utf8(unsafe { text_range_as_bytes(&self.raw.userInfo) }.unwrap()).unwrap()
+    pub fn userinfo(&self) -> Option<&str> {
+        unsafe { text_range_as_bytes(&self.raw.userInfo) }.map(|s| core::str::from_utf8(s).unwrap())
     }
-    pub fn host(&self) -> &str {
-        core::str::from_utf8(unsafe { text_range_as_bytes(&self.raw.hostText) }.unwrap()).unwrap()
+    pub fn host(&self) -> Option<&str> {
+        unsafe { text_range_as_bytes(&self.raw.hostText) }.map(|s| core::str::from_utf8(s).unwrap())
     }
-    pub fn query(&self) -> &str {
-        core::str::from_utf8(unsafe { text_range_as_bytes(&self.raw.query) }.unwrap()).unwrap()
+    pub fn port(&self) -> Option<&str> {
+        unsafe { text_range_as_bytes(&self.raw.portText) }.map(|s| core::str::from_utf8(s).unwrap())
+    }
+    pub fn path(&self) -> Path<'_> {
+        let head = core::ptr::NonNull::new(self.raw.pathHead).map(|ptr| unsafe { &*ptr.as_ptr() });
+        let is_absolute = self.raw.absolutePath == uriparser_sys::URI_TRUE as i32 || (self.host().is_some() && head.is_some());
+        Path {
+            is_absolute,
+            head
+        }
+    }
+
+    pub fn query(&self) -> Option<&str> {
+        unsafe { text_range_as_bytes(&self.raw.query) }.map(|s| core::str::from_utf8(s).unwrap())
+    }
+    pub fn fragment(&self) -> Option<&str> {
+        unsafe { text_range_as_bytes(&self.raw.fragment) }.map(|s| core::str::from_utf8(s).unwrap())
     }
 }
-
+impl PartialEq<Uri<'_>> for Uri<'_> {
+    fn eq(&self, other: &Uri<'_>) -> bool {
+        (unsafe {
+            uriparser_sys::uriEqualsUriA(&self.raw, &other.raw)
+        }) as u32 == uriparser_sys::URI_TRUE
+    }
+}
+impl ToString for Uri<'_> {
+    fn to_string(&self) -> String {
+        let mut capacity = 0;
+        assert_eq!(unsafe {
+            uriToStringCharsRequiredA(&self.raw, &mut capacity)
+        } as u32, uriparser_sys::URI_SUCCESS);
+        let mut s = String::with_capacity(capacity as usize);
+        let mut written = 0;
+        assert_eq!(unsafe {
+            uriToStringA(s.as_bytes_mut().as_mut_ptr() as *mut _, &self.raw, capacity, &mut written)
+        } as u32, uriparser_sys::URI_SUCCESS);
+        unsafe {
+            s.as_mut_vec().set_len(written as usize);
+        }
+        s
+    }
+}
+pub struct Path<'a> {
+    is_absolute: bool,
+    head: Option<&'a UriPathSegmentA>,
+}
+use fmt::Write;
+impl fmt::Debug for Path<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_char('"')?;
+        if self.is_absolute {
+            f.write_char('/')?;
+        }
+        let mut segments = self.segments();
+        if let Some(segment) = segments.next() {
+            write!(f, "{}", segment.escape_debug())?;
+            for segment in segments {
+                f.write_char('/')?;
+                write!(f, "{}", segment.escape_debug())?;
+            }
+        }
+        Ok(())
+    }
+}
+impl cmp::PartialEq<[u8]> for Path<'_> {
+    fn eq(&self, mut other: &[u8]) -> bool {
+        macro_rules! unwrap {
+            ($e:expr) => {match $e {
+                Some(v) => v,
+                None => return false
+            }};
+        }
+        if self.is_absolute {
+            other = unwrap!(other.strip_prefix(b"/"));
+        }
+        let mut segments = self.segments();
+        if let Some(segment) = segments.next() {
+            other = unwrap!(other.strip_prefix(segment.as_bytes()));
+            for segment in segments {
+                other = unwrap!(other.strip_prefix(b"/"));
+                other = unwrap!(other.strip_prefix(segment.as_bytes()));
+            }
+        }
+        true
+    }
+}
+impl cmp::PartialEq<str> for Path<'_> {
+    fn eq(&self, other: &str) -> bool {
+        self.eq(other.as_bytes())
+    }
+}
+impl cmp::PartialEq for Path<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.is_absolute.eq(&other.is_absolute) && self.segments().eq(other.segments())
+    }
+}
+impl fmt::Display for Path<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.is_absolute {
+            f.write_char('/')?;
+        }
+        let mut segments = self.segments();
+        if let Some(segment) = segments.next() {
+            f.write_str(segment)?;
+            for segment in segments {
+                f.write_char('/')?;
+                f.write_str(segment)?;
+            }
+        }
+        Ok(())
+    }
+}
+impl<'a> Path<'a> {
+    pub fn segments(&self) -> impl Iterator<Item = &'a str> + 'a {
+        let mut next = self.head;
+        core::iter::from_fn(move || {
+            next.map(|segment| {
+                next = core::ptr::NonNull::new(segment.next).map(|ptr| unsafe { &*ptr.as_ptr() });
+                core::str::from_utf8(unsafe { text_range_as_bytes(&segment.text) }.unwrap()).unwrap()
+            })
+        })
+    }
+}
 impl Uri<'_> {
     fn into_raw(self) -> uriparser_sys::UriUriA {
         self.raw
@@ -103,5 +316,17 @@ impl Drop for Uri<'_> {
         unsafe {
             uriparser_sys::uriFreeUriMembersA(&mut self.raw);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Uri;
+
+    #[test]
+    fn parsing() {
+        let uri = Uri::parse(b"https://www.youtube.com/watch?v=HOJ1NVtlnyQ").unwrap();
+        assert_eq!(Some("v=HOJ1NVtlnyQ"), uri.query());
+        Uri::parse(b"foobar://abc.com/ooh##").expect_err("no hashes in fragment");
     }
 }
